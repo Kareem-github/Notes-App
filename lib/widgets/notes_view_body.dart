8021f48efd768df1b8475260270083fe3cd895f6
Kareem Sobhi <kareemsobhi50@gmail.com>
2023-01-29 13:41:38 +0200

    custom note item and app bar

---
+++ lib/widgets/notes_view_body.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/custom_app_bar.dart';
+import 'package:notes_app/widgets/custom_notes_item.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 18),
+      child: Column(
+        children: const [
+          SizedBox(height: 30,),
+          CustomAppBar(),
+          NoteItem(),
+        ],
+      ),
+    );
+  }
+}
+
